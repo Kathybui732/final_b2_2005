7649025176329018215d363c50c02e2d43a7033f
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-03 11:08:12 -0600

    Add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/passengers/:id", to: 'passengers#show'
+  # get "/passengers/:id", to: 'passengers#show'
+  resources :passengers, only: [:show]
 
-  get "/flights/:id", to: 'flights#show'
+  # get "/flights/:id", to: 'flights#show'
+  resources :flights, only: [:show]
 
   post "/passengers/:passenger_id/flight_passengers", to: 'flight_passengers#create'
 end
